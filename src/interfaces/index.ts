ff28cc2a72c5cab9e9e56f9546f3ee7464348942
sonchaegeon <thscorjs1002@naver.com>
2021-04-22 20:00:49 +0900

    UPDATE; lol arg interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,26 @@
+type ContinentRegion = 'asia' | 'americas' | 'esports' | 'europe';
+
+type Region =
+  | 'br1'
+  | 'eun1'
+  | 'euw1'
+  | 'kr'
+  | 'jp1'
+  | 'la1'
+  | 'la2'
+  | 'na1'
+  | 'oc1'
+  | 'ru'
+  | 'tr1';
+
 interface IRiotArg {
   apiKey: string;
+  region: ContinentRegion;
 }
 
-export { IRiotArg };
+interface ILolArg {
+  apiKey: string;
+  region: Region;
+}
+
+export { IRiotArg, ILolArg };
