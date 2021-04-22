641936d05919e9e46c58b3617618d82176fbaf2f
sonchaegeon <thscorjs1002@naver.com>
2021-04-22 14:02:20 +0900

    UPDATE; get summoner by account test

--- src/test/riot.spec.ts
+++ src/test/riot.spec.ts
@@ -2,12 +2,23 @@ import Riot from '../riot';
 
 describe('node-riotapi', () => {
   const riot = new Riot({
-    apiKey: '',
+    apiKey: 'RGAPI-7d85968f-3386-481d-a88e-33cebb64130a',
   });
 
   describe('summoner.getSummonerByName', () => {
     it('should be return summoner', async () => {
       const summonerInfo = await riot.getSummonerByName('대덕sw마이스터고');
+      expect(summonerInfo).toBeDefined();
+      expect(summonerInfo.name).toBe('대덕SW마이스터고');
+      expect(summonerInfo.profileIconId).toBe(23);
+    });
+  });
+
+  describe('summoner.getSummonerByAccount', () => {
+    it('should be return summoner', async () => {
+      const summonerInfo = await riot.getSummonerByAccount(
+        '5S842iiXcGAN5T7xQMSskgrUhKBLX8Oz-n9hv492cTDBvZY',
+      );
       expect(summonerInfo).toBeDefined();
       expect(summonerInfo.name).toBe('대덕SW마이스터고');
       expect(summonerInfo.profileIconId).toBe(23);
