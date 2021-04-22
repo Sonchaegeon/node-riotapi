186bc23c31eddf5def4a840e1f69daa672477a60
sonchaegeon <thscorjs1002@naver.com>
2021-04-22 20:01:37 +0900

    UPDATE; get account by puuid example

--- src/example/index.ts
+++ src/example/index.ts
@@ -1,33 +1,51 @@
-import Riot from '../riot';
+import { Riot, Lol } from '..';
+
+const apiKey = 'RGAPI-9401e89e-758b-4c52-9a6b-7e2de0758e25';
 
 const riot = new Riot({
-  apiKey: 'RGAPI-7d85968f-3386-481d-a88e-33cebb64130a',
+  apiKey,
+  region: 'asia',
 });
 
+const lol = new Lol({
+  apiKey,
+  region: 'kr',
+});
+
+// Summoner
 async function getSummonerByName() {
-  const summonerInfo = await riot.getSummonerByName('대덕sw마이스터고');
+  const summonerInfo = await lol.getSummonerByName('대덕sw마이스터고');
   console.log(summonerInfo);
 }
 
 async function getSummonerByAccount() {
-  const summonerInfo = await riot.getSummonerByAccount(
+  const summonerInfo = await lol.getSummonerByAccount(
     '5S842iiXcGAN5T7xQMSskgrUhKBLX8Oz-n9hv492cTDBvZY',
   );
   console.log(summonerInfo);
 }
 
 async function getSummonerByPuuid() {
-  const summonerInfo = await riot.getSummonerByPuuid(
+  const summonerInfo = await lol.getSummonerByPuuid(
     'SHNBy0tWOTMK5Z0TBLcZ4y9HVkitJejOzVo_uVeqXQPomBjWKu3NIiekWAF59nBGQiTBK9xp8fYR7Q',
   );
   console.log(summonerInfo);
 }
 
 async function getSummoner() {
-  const summonerInfo = await riot.getSummoner(
+  const summonerInfo = await lol.getSummoner(
     'zTzeGkZR7C9RwzW_9xVMC2068P3HKJManaUwgZVisQ1iBBU',
   );
   console.log(summonerInfo);
 }
 
-getSummoner();
+// Account
+async function getAccountByPuuid() {
+  const accountInfo = await riot.getAccountByPuuid(
+    'SHNBy0tWOTMK5Z0TBLcZ4y9HVkitJejOzVo_uVeqXQPomBjWKu3NIiekWAF59nBGQiTBK9xp8fYR7Q',
+  );
+  console.log(accountInfo);
+}
+
+getSummonerByName();
+getAccountByPuuid();
