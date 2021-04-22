3a19ed92a9463d06494cf5e4cd5afa7979fc5584
sonchaegeon <thscorjs1002@naver.com>
2021-04-22 13:41:01 +0900

    CREATE; summoner-v4 interface

---
+++ src/interfaces/response/summoner-v4.ts
@@ -0,0 +1,11 @@
+interface IGetSummonersByNameRes {
+  id: string;
+  accountId: string;
+  puuid: string;
+  name: string;
+  profileIconId: number;
+  revisionDate: Date;
+  summonerLevel: number;
+}
+
+export { IGetSummonersByNameRes };
